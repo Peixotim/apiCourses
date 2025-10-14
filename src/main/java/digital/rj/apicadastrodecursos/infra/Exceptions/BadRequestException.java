7aa30b20c8dcf2e729653cc031d696d90523f208
Peixotim <pedropeixotovz@gmail.com>
2025-10-14 17:40:36 -0300

    feat

--- src/main/java/digital/rj/apicadastrodecursos/infra/Exceptions/BadRequestException.java
+++ src/main/java/digital/rj/apicadastrodecursos/infra/Exceptions/BadRequestException.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
+
     public BadRequestException(){
         super("BadRequest, unhandled error!");
     }
+
     public BadRequestException(String message) {
         super(message);
     }
